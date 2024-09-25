41e781743f82a42ef41d6a098f98bd03bcc359f2
AsumaToki04 <megamanzxu@hotmail.com>
2024-09-25 10:15:45 -0500

    Ejercicio Completado

---
+++ src/main/java/pe/edu/cibertec/app_clienterest_basic/controller/UserController.java
@@ -0,0 +1,22 @@
+package pe.edu.cibertec.app_clienterest_basic.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.cibertec.app_clienterest_basic.client.placeholder.model.User;
+import pe.edu.cibertec.app_clienterest_basic.service.UserService;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/usuario/{id}")
+    public User obtenerUsuarioXid(@PathVariable("id") Long id) {
+        return userService.obtenerUsuarioXId(id);
+    }
+
+    @PostMapping("/usuario")
+    public User crearUsuario(@RequestBody User usuario) {
+        return userService.crearUsuario(usuario);
+    }
+}
