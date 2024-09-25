12d401f8215e5366bf25acb8d9da945a43aadc40
AsumaToki04 <megamanzxu@hotmail.com>
2024-09-25 10:02:53 -0500

    Proyecto Creado

---
+++ src/main/java/pe/edu/cibertec/app_clienterest_basic/AppClienterestBasicApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.app_clienterest_basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppClienterestBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppClienterestBasicApplication.class, args);
+	}
+
+}
